a1fd7b929dc8e582751dcb1caec03273f33745ce
Gamzatov_R <gamzatovrustam28@gmail.com>
2024-11-22 18:08:51 +0300

    уххх ты

---
+++ Ходунки C#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hodunki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 45;
+            Console.WriteLine(a);
+        }
+    }
+}
