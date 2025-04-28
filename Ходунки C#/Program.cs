49d0d2652716982bb10f1d9603924d1f9232827e
Gamzatov_R <gamzatovrustam28@gmail.com>
2025-04-28 09:48:22 +0300

    first

--- Ходунки C#/Program.cs
+++ Ходунки C#/Program.cs
@@ -4,10 +4,19 @@ namespace Hodunki
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int a = 45;
-            Console.WriteLine(a);
+            void GetRectangleData(in int width, int height, out int rectArea, out int rectPerimetr)
+            {
+                width = 25; // нельзя изменить, так как width - входной параметр
+                rectArea = width * height;
+                rectPerimetr = (width + height) * 2;
+            }
+
+            GetRectangleData(10, 20, out var area, out var perimetr);
+
+            Console.WriteLine($"Площадь прямоугольника: {area}");       // 200
+            Console.WriteLine($"Периметр прямоугольника: {perimetr}");   // 60
         }
     }
 }
