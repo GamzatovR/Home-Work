60672ae5353470a2f64aa113f7ea4e4746ecfb24
GamzatovR <gamzatovrustam28@gmail.com>
2025-05-03 13:44:57 +0300

    Add files via upload

---
+++ CombSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace PancakeSortt
+{
+    internal class CombSort
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            GeneratorData.Generator();
+            string directoryPath = "InputData";
+            string[] files = Directory.GetFiles(directoryPath, "*.txt");
+            foreach (string file in files)
+            {
+                string data = File.ReadAllText(file);
+                string[] numberStr = data.Split(' ');
+                int[] array = Array.ConvertAll(numberStr, int.Parse);
+                Console.WriteLine($"Сортировка массива из {array.Length} элеметов");
+
+                var stopWatch = Stopwatch.StartNew();
+                Sort(array);
+                stopWatch.Stop();
+
+                Console.WriteLine($"Время сортировки в милисекундах: {stopWatch.ElapsedMilliseconds } ");
+                
+            }
+        }
+        public static void Sort(int[] arr)
+        {
+            int passes = 0; // кол-во проходов
+            int comparisons = 0; // кол-во сравнений
+            int swaps = 0; //кол-во обменов
+
+            int lenArray = arr.Length; // длина массива
+            int gap = lenArray;
+            bool swapped = true; // Это типа флаг была ли перестановка элеметов
+
+            while (gap != 1 || swapped)
+            {
+                passes++;
+                gap = Math.Max(1,(int)(gap / 1.247)); // Расстояние между проверкой элементов по индексу
+                swapped = false;
+                for (int i = 0; i < lenArray - gap; i++)
+                {
+                    comparisons++;
+                    if (arr[i] > arr[i + gap])
+                    {
+                        swaps++;
+                        int temp = arr[i];
+                        arr[i] = arr[i + gap];
+                        arr[i + gap] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+            Console.WriteLine($"Кол-во проходов: {passes}");
+            Console.WriteLine($"Кол-во сравнений: {comparisons}");
+            Console.WriteLine($"Кол-во обменов: {swaps}");
+        }
+    }
+}
---
+++ GeneratorData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PancakeSortt
+{
+    internal class GeneratorData
+    {
+        public static void Generator()
+        {
+            string directoryPath = "InputData"; //Это папка данных
+            Directory.CreateDirectory(directoryPath); // Это создаём саму папку
+
+            Random random = new Random();
+            int dataSetCount = 50; // Это кол-во наборов данных
+            for (int i = 0; i < dataSetCount; i++)
+            {
+                int size = random.Next(100, 10000); //Кол-во элеметов в наборе
+                int[] array = new int[size];
+                for (int j = 0; j < size; j++)
+                {
+                    array[j] = random.Next(10000); // На каждый элемент, создаётся число диапазоном от 0 до 10 000
+                }
+                string filePath = Path.Combine(directoryPath, $"dataset_{i + 1}.txt"); //Путь к файлу
+                File.WriteAllText(filePath, string.Join(" ", array)); // Создаётся текстовый файл
+                Console.WriteLine($"Создан dataset_{i + 1}.txt с {size} количесвом элементов");
+            }
+            Console.WriteLine("Данные сгенерированы");
+        }
+    }
+}
